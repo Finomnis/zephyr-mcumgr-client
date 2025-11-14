b9710880ff6f5b966b63731d471ab82e948edcbf
Finomnis <finomnis@gmail.com>
2025-11-15 00:54:31 +0100

    Add longer echo test

--- zephyr-mcumgr/tests/integration_tests.rs
+++ zephyr-mcumgr/tests/integration_tests.rs
@@ -1,5 +1,6 @@
 mod common;
 use common::EchoSerial;
+use rand::prelude::*;
 use zephyr_mcumgr::MCUmgrClient;
 
 #[test]
@@ -8,6 +9,13 @@ fn echo() {
 
     let request = "Hello world!";
     let response = client.os_echo(request).unwrap();
+    assert_eq!(request, response);
 
+    let request: String = rand::rng()
+        .sample_iter(&rand::distr::Alphanumeric)
+        .take(10000)
+        .map(char::from)
+        .collect();
+    let response = client.os_echo(&request).unwrap();
     assert_eq!(request, response);
 }
