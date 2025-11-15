66bb9415aa5d1ce9d05406f80593c9e4dd93c22c
Finomnis <Finomnis@users.noreply.github.com>
2025-11-15 01:21:04 +0100

    Add some tests (#43)

---
+++ zephyr-mcumgr/tests/integration_tests.rs
@@ -0,0 +1,21 @@
+mod common;
+use common::EchoSerial;
+use rand::prelude::*;
+use zephyr_mcumgr::MCUmgrClient;
+
+#[test]
+fn echo() {
+    let mut client = MCUmgrClient::new_from_serial(EchoSerial::default());
+
+    let request = "Hello world!";
+    let response = client.os_echo(request).unwrap();
+    assert_eq!(request, response);
+
+    let request: String = rand::rng()
+        .sample_iter(&rand::distr::Alphanumeric)
+        .take(10000)
+        .map(char::from)
+        .collect();
+    let response = client.os_echo(&request).unwrap();
+    assert_eq!(request, response);
+}
